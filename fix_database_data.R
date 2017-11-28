013870b8f2fe75721c723bb39c0bb9e793fdb460
dinasinclair <dinacrs@gmail.com>
2017-11-28 14:58:42 +0000

    created first attempt excel and SPSS output files to check with Mady

--- fix_database_data.R
+++ fix_database_data.R
@@ -1,6 +1,10 @@
 
 # Read in csv, first making sure NA is read as NA and doesn't turn the column into a string!
+# Checknames is false because some of the var names in input are not valid var names in R, so 
+# if you use checknames it turns the (,),| into periods and it looks weird plus messes with the variable
+# name shortening below
 data <- read.csv("Watson/Tostan/Forms.csv", na.strings = '---', check.names=FALSE)
+old_names <- names(data)
 
 # Rename variable names. We need to only take what comes after the last period.
 # I should print out a list of the old/new variable names and compare?
@@ -9,10 +13,17 @@ names(data) <-  sub(".*\\.", "", names(data))
 
 #write.csv(data, file = "r-prettified.csv")
 library(xlsx)
-write.xlsx(x = data, file = "r-prettified-excel.xlsx", sheetName = "TestSheet", row.names = FALSE)
-
+write.xlsx(x = data, file = "Watson/Tostan/r-data-test-excel.xlsx", sheetName = "TestSheet", row.names = FALSE)
+print("wrote to excel")
 
 # write out text datafile and
 # an SPSS program to read it
 library(foreign)
-write.foreign(data, "r-data.txt", "r-data.sps",   package="SPSS")
+write.foreign(data, "Watson/Tostan/r-data-test.txt", "Watson/Tostan/r-data-test.sps",   package="SPSS")
+print("wrote to SPSS")
+
+# What if I want to check and see if the variable names are the same/correct in the new version?
+print("check variable names")
+new_names <- names(data)
+check_variable_names <- data.frame(old_names,new_names)
+
